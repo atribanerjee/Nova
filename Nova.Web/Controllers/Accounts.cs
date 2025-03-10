8b49568becdcdd01a7e2b4f795632dc2d37b2933
Atri Banerjee <atribanerjee@hotmail.com>
2025-03-11 02:52:13 +0530

    Add project files.

---
+++ Nova.Web/Controllers/Accounts.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nova.Web.Controllers
+{
+    public class Accounts : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
